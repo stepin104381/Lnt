682aaf5f7b93b6f48949d534104227e84faa3097
stepin104381 <nisarga0399@gmail.com>
2020-10-27 16:20:50 +0530

    Adding Log

--- app/src/main/java/com/example/lnt/HomeActivity.java
+++ app/src/main/java/com/example/lnt/HomeActivity.java
@@ -3,12 +3,15 @@ package com.example.lnt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    public static String TAG=HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Log.i(TAG,"Activity created");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         String data=getIntent().getExtras().getString("mykey");
--- app/src/main/java/com/example/lnt/MainActivity.java
+++ app/src/main/java/com/example/lnt/MainActivity.java
@@ -4,25 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText nameEditText;
+    public static String TAG=MainActivity.class.getSimpleName();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"Activity Created");
         nameEditText=findViewById(R.id.editTextTextPersonName);
     }
 
     public void clickHandler(View view) {
+        Log.i(TAG,"Button Clicked");
         switch (view.getId())
         {
             case R.id.buttonlogin:
+                Log.w(TAG,"logging in");
                 String name=nameEditText.getText().toString();
                // Toast.makeText(this, "Welcome to android! "+name, Toast.LENGTH_SHORT).show();
                 Intent homeIntent=new Intent(MainActivity.this,HomeActivity.class);
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 break;
             case R.id.buttoncancel:
+                Log.e(TAG,"cancelling");
                 Toast.makeText(this, "Cancelled ! ", Toast.LENGTH_SHORT).show();
                 break;
 
